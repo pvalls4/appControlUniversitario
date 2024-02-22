b79167e9c09332f29a7c30877b78864950434cda
Adrian Blanco <ablancodevelop@gmail.com>
2024-02-22 13:33:51 +0100

    Pongo cosas

--- AppControlUniversitario_PauVAdri/src/model/persist/CarreraDao.java
+++ AppControlUniversitario_PauVAdri/src/model/persist/CarreraDao.java
@@ -4,10 +4,29 @@
  */
 package model.persist;
 
+import java.sql.*;
+import model.Carrera;
+
+
 /**
  *
  * @author Mati
  */
 public class CarreraDao {
+
+    private final DbConnect dbConnect;
     
+    public CarreraDao(){
+        dbConnect = new DbConnect();
+    }
+    
+    private Carrera fromResultSet(ResultSet rs) throws SQLException{
+        Carrera carrera;
+        //Leemos los atributos de carrera
+        int idCarrera = rs.getInt("id");
+        String nombreCarrera = rs.getString("nombre");
+        //Instanciamos un nuevo objeto Carrera con los datos obtenidos
+        carrera = new Carrera(idCarrera, nombreCarrera);
+        return carrera;
+    }
 }
--- AppControlUniversitario_PauVAdri/src/model/persist/DbConnect.java
+++ AppControlUniversitario_PauVAdri/src/model/persist/DbConnect.java
@@ -4,10 +4,41 @@
  */
 package model.persist;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
+ * encapsulates data for database connection.
  *
- * @author Mati
+ * @author ProvenSoft
  */
-public class DbConnect {
-    
+public final class DbConnect {
+
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String PROTOCOL = "jdbc:mysql:";
+    public static final String HOST = "127.0.0.1";
+    public static final String BD_NAME = "bd_institucion";
+    public static final String USER = "root";
+    public static final String PASSWORD = "123456";
+    public static String BD_URL;
+
+    public static void loadDriver() throws ClassNotFoundException {
+        //getConnectionProperties(); better if connection properties are read from a configuration file
+        Class.forName(DRIVER);
+        BD_URL = String.format("%s//%s/%s", PROTOCOL, HOST, BD_NAME);
+    }
+
+    /**
+     * gets and returns a connection to database
+     *
+     * @return connection
+     * @throws java.sql.SQLException
+     */
+    public Connection getConnection() throws SQLException {
+        BD_URL = String.format("%s//%s/%s", PROTOCOL, HOST, BD_NAME);
+        Connection conn;
+        conn = DriverManager.getConnection(BD_URL, USER, PASSWORD);
+        return conn;
+    }
 }
