f19461d67526a0ebc38a5d77eaf1158b333d1ac8
pvalls4 <valls.vilalta.4@gmail.com>
2024-02-22 14:28:17 +0100

    AlumnoDao updated

--- AppControlUniversitario_PauVAdri/src/model/persist/AlumnoDao.java
+++ AppControlUniversitario_PauVAdri/src/model/persist/AlumnoDao.java
@@ -4,10 +4,75 @@
  */
 package model.persist;
 
-/**
- *
- * @author Mati
- */
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import model.Alumno;
+
 public class AlumnoDao {
+
+    private final DbConnect dbConnect;
     
+    public AlumnoDao(){
+        dbConnect = new DbConnect();
+    }
+    
+    private Alumno fromResultSet(ResultSet rs) throws SQLException{
+        Alumno alumno;
+        //Leemos los atributos de carrera
+        int id = rs.getInt("id");
+        String nombre = rs.getString("nombre");
+        String apellido = rs.getString("apellido");
+        String email = rs.getString("email");
+        int idCarrera = rs.getInt("idCarrera");
+        //Instanciamos un nuevo objeto Carrera con los datos obtenidos
+        alumno = new Alumno(id, nombre, apellido, email, idCarrera);
+        return alumno;
+    }
+    
+    public List<Alumno> listarCarreras(){
+        List<Alumno> result = new ArrayList<>();
+        try (Connection conn = dbConnect.getConnection()) {
+            //Si la conexión es exitosa
+            if (conn != null) {
+                String query = "SELECT * FROM alumnos;";
+                Statement st = conn.createStatement();
+                ResultSet rs = st.executeQuery(query);
+                while (rs.next()) {
+                    Alumno alumno = fromResultSet(rs);
+                    //Comprovamos que la carrera sea válida
+                    if (alumno != null) {
+                        result.add(alumno);
+                    }
+                }
+            }
+        } catch (SQLException ex) {
+            result = null;
+        }
+        return result;
+    }
+    
+    public Alumno buscarALumno(int id){
+        Alumno result = null;
+        
+        return result;
+    }
+    
+    public int agregarAlumno(Alumno alumno){
+        int result = 0;
+        
+        return result;
+    }
+    
+    public int modificarAlumno(Alumno oldCarrera, Alumno updatedCarrera){
+        int result = 0;
+        
+        return result;
+    }
+    
+    public int eliminarAlumno(Alumno carrera){
+        int result = 0;
+        
+        return result;
+    }
 }
