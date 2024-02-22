b64f8cbc362bf4d66d5f9c586a3116367cba16c6
pvalls4 <valls.vilalta.4@gmail.com>
2024-02-22 13:29:10 +0100

    Menú gestión de alumnos

--- AppControlUniversitario_PauVAdri/src/view/Menu.java
+++ AppControlUniversitario_PauVAdri/src/view/Menu.java
@@ -19,6 +19,7 @@ public class Menu {
         System.out.println("============== APP CONTROL UNIVERSITARIO ==============");
         System.out.println("    0. Salir");
         System.out.println("    1. Gestionar Carreras");
+        System.out.println("    2. Gestionar Alumnos");
         try {
             result = sc.nextInt();
         } catch (InputMismatchException e) {
@@ -27,16 +28,34 @@ public class Menu {
         return result;
     }
     
-    public int showMenuCarrera(){
+    public int showMenuCarreras(){
         Scanner sc = new Scanner(System.in);
         int result = -1;
         System.out.println("============== GESTIÓN DE CARRERAS ==============");
         System.out.println("    0. Atrás");
-        System.out.println("    1. Listar Todas las Carreras");
-        System.out.println("    2. Listar una Carrera");
+        System.out.println("    1. Listar todas las Carreras");
+        System.out.println("    2. Buscar una Carrera existente");
         System.out.println("    3. Modificar una Carrera");
         System.out.println("    4. Añadir una Carrera");
         System.out.println("    5. Eliminar una Carrera");
+        try {
+            result = sc.nextInt();
+        } catch (InputMismatchException e) {
+            result = showMenu();
+        }
+        return result;
+    }
+    
+    public int showMenuAlumnos(){
+        Scanner sc = new Scanner(System.in);
+        int result = -1;
+        System.out.println("============== GESTIÓN DE ALUMNOS ==============");
+        System.out.println("    0. Atrás");
+        System.out.println("    1. Listar todos los Alumnos");
+        System.out.println("    2. Buscar un Alumno existente");
+        System.out.println("    3. Modificar un Alumno");
+        System.out.println("    4. Añadir un Alumno");
+        System.out.println("    5. Eliminar un Alumno");
         try {
             result = sc.nextInt();
         } catch (InputMismatchException e) {
